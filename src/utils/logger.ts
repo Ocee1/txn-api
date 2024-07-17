2fed18e1215ebe592d2e3c539fd02306b80a8229
Ocee <ocjay24@gmail.com>
2024-07-17 09:49:11 +0100

    modified logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -15,35 +15,11 @@ const logFormat = winston.format.printf(({ timestamp, level, message }) => `${ti
 
 
 const logger = winston.createLogger({
-    format: winston.format.combine(
-        winston.format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss',
-        }),
-        logFormat,
-    ),
     transports: [
-        // debug log setting
-        new winstonDaily({
-            level: 'debug',
-            datePattern: 'YYYY-MM-DD',
-            dirname: logDir + '/debug', 
-            filename: `%DATE%.log`,
-            maxFiles: 30, // 30 Days saved
-            json: true,
-            zippedArchive: true,
-        }),
-        // error log setting
-        new winstonDaily({
-            level: 'error',
-            datePattern: 'YYYY-MM-DD',
-            dirname: logDir + '/error', 
-            filename: `%DATE%.log`,
-            maxFiles: 30, // 30 Days saved
-            handleExceptions: true,
-            json: true,
-            zippedArchive: true,
-        }),
-    ],
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        })
+    ]
 });
 
 logger.add(
