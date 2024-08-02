2439f70ebe0bfb7cbb11008609a3079ee09b8ee7
Ocee <ocjay24@gmail.com>
2024-08-02 06:47:33 +0100

    update to models

--- src/models/user.ts
+++ src/models/user.ts
@@ -20,28 +20,7 @@ class User extends BaseModel {
   createdAt?: Date;
   updatedAt?: Date;
 
-  // static get columnNameMappers() {
-  //   return {
-  //     parseDatabaseJson: (json: any) => {
-  //       if (json.created_at) {
-  //         json.createdAt = new Date(json.created_at);
-  //       }
-  //       if (json.updated_at) {
-  //         json.updatedAt = new Date(json.updated_at);
-  //       }
-  //       return json;
-  //     },
-  //     formatDatabaseJson: (json: any) => {
-  //       if (json.createdAt) {
-  //         json.created_at = json.createdAt.toISOString();
-  //       }
-  //       if (json.updatedAt) {
-  //         json.updated_at = json.updatedAt.toISOString();
-  //       }
-  //       return json;
-  //     },
-  //   } as any; 
-  // }
+
 
 }
 
